77911084ca1307436ba733f248c5af4d4a74ba2f
Miguel Luizatto Alves <131935219+l3gium@users.noreply.github.com>
2024-11-20 14:51:38 -0300

    Add files via upload

---
+++ WindowsFormsApp1/Guiches.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Guiches
+    {
+        private List<Guiche> listaGuiches;
+
+        public Guiches()
+        {
+            this.listaGuiches = new List<Guiche>();
+        }
+
+        public void adicionar(Guiche guiche)
+        {
+            this.listaGuiches.Add(guiche);
+        }
+
+        public List<Guiche> getListaGuiches()
+        {
+            return this.listaGuiches;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Senha.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Senha
+    {
+        private int id;
+        private DateTime dataGerac, horaGerac, dataAtend, horaAtend;
+
+        public Senha(int id)
+        {
+            this.id = id;
+            dataGerac = DateTime.Now;
+            horaGerac = DateTime.Now;
+        }
+
+        public string dadosParciais()
+        {
+            return this.id + " - " + dataGerac.ToString() + " - " + horaGerac.ToString();
+        }
+
+        public string dadosCompletos()
+        {
+            return dadosParciais() + " - " + dataAtend.ToString() + " - " + horaAtend.ToString(); 
+        }
+    }
+}
